a92533b729a9a6fa2bce64e153bd28dad2cbe95f
zak aden <s.aden159@gmail.com>
2026-01-08 23:38:24 +0000

    Initial Commit

---
+++ ImanPath/ImanPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImanPathApp.swift
+//  ImanPath
+//
+//  Created by zak aden on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ImanPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImanPathTests/ImanPathTests.swift
@@ -0,0 +1,16 @@
+//
+//  ImanPathTests.swift
+//  ImanPathTests
+//
+//  Created by zak aden on 08/01/2026.
+//
+
+import Testing
+
+struct ImanPathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
